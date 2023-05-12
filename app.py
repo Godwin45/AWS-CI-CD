843ecb7685b61dda8acb00bb38686480019d871a
Godwin45 <71969710+Godwin45@users.noreply.github.com>
2023-05-12 19:23:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
 
+
+
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
